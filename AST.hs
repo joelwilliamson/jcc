a440db0b7e21f241306753d6b6a22cb9787bdd20
Joel Williamson <joel.s.williamson@gmail.com>
2015-02-02 13:41:01 -0500

    The basic types for C expressions

---
+++ AST.hs
@@ -0,0 +1,102 @@
+module AST where
+
+data Identifier = Identifier String
+         deriving (Eq,Show)
+
+data Constant = Integer Integer
+              | Float Double
+              | Char Char
+              | Enumeration Identifier
+              deriving (Eq,Show)
+
+data StringLiteral = StringLiteral String
+                     deriving (Eq,Show)
+
+data Expression = AssignmentExpression AssignmentExpression
+                | Comma Expression AssignmentExpression
+
+data AssignmentExpression = ConditionalExpression ConditionalExpression
+                          | NestedAssignmentExpression UnaryExpression AssignmentOperator AssignmentExpression
+
+data AssignmentOperator = SimpleAssign
+                        | MultiplyAssign
+                        | DivideAssign
+                        | ModulusAssign
+                        | PlusAssign
+                        | MinusAssign
+                        | LShiftAssign
+                        | RShiftAssign
+                        | AndAssign
+                        | XorAssign
+                        | OrAssign
+
+data ConditionalExpression = LogicalOrExpression LogicalOrExpression
+                           | Ternary LogicalOrExpression Expression ConditionalExpression
+
+data LogicalOrExpression = LogicalAndExpression LogicalAndExpression
+                         | LogicalOr LogicalOrExpression LogicalAndExpression
+
+data LogicalAndExpression = BitwiseOrExpression BitwiseOrExpression
+                          | LogicalAnd LogicalAndExpression BitwiseOrExpression
+
+data BitwiseOrExpression = BitwiseXorExpression BitwiseXorExpression
+                         | BitwiseOr BitwiseOrExpression BitwiseXorExpression
+
+data BitwiseXorExpression = BitwiseAndExpression BitwiseAndExpression
+                          | Xor BitwiseXorExpression BitwiseAndExpression
+
+data BitwiseAndExpression = EqualityExpression EqualityExpression
+                          | BitwiseAnd BitwiseAndExpression EqualityExpression
+
+data EqualityExpression = RelationalExpression RelationalExpression
+                        | Equal EqualityExpression RelationalExpression
+                        | DisEqual EqualityExpression RelationalExpression
+
+data RelationalExpression = ShiftExpression ShiftExpression
+                          | CompareOperator RelationalExpression ShiftExpression
+
+data CompareOperator = LT | GT | LTE | GTE
+
+data ShiftExpression = AdditiveExpression AdditiveExpression
+                     | LShift ShiftExpression AdditiveExpression
+                     | RShift ShiftExpression AdditiveExpression
+
+data AdditiveExpression = MultiplicativeExpression MultiplicativeExpression
+                        | Add AdditiveExpression MultiplicativeExpression
+                        | Subtract AdditiveExpression MultiplicativeExpression
+
+data MultiplicativeExpression = CastExpression CastExpression
+                              | Multiply MultiplicativeExpression CastExpression
+                              | Divide MultiplicativeExpression CastExpression
+                              | Modulus MultiplicativeExpression CastExpression
+
+data CastExpression = UnaryExpression UnaryExpression
+                    | NestedCastExpression Identifier CastExpression
+
+data UnaryExpression = PostfixExpression PostfixExpression
+                     | PreIncrement UnaryExpression
+                     | PreDecrement UnaryExpression
+                     | UnaryOp UnaryOp CastExpression
+                     | ExpressionSize UnaryExpression
+                     | TypeSize Identifier
+
+data UnaryOp = AddressOf | Dereference | UnaryPlus | UnaryMinus | BitwiseNot | LogicalNot
+
+data PostfixExpression = PrimaryExpression PrimaryExpression
+                       | ArraySubscript PostfixExpression Expression
+                       | FunctionCall PostfixExpression ArgumentExpressionList
+                       | Member PostfixExpression Identifier
+                       | PointerToMember PostfixExpression Identifier
+                       | PostIncrement PostfixExpression
+                       | PostDecrement PostfixExpression
+                       | CompoundLiteral Identifier InitializerList
+
+data ArgumentExpressionList = ArgListHead AssignmentExpression
+                            | ArgList ArgumentExpressionList AssignmentExpression
+
+data PrimaryExpression = IdentifierExpression Identifier
+                       | Constant Constant
+                       | StringLiteralExpression StringLiteral
+                       | Expr Expression
+
+type InitializerList = () -- See section 6.7.8 for details
